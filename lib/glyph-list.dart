2524386f2ee3986f4a389beba268a75063233a27
gmendozah <geovanimendoza.h@gmail.com>
2021-07-16 18:22:56 -0400

    feat: add video and webview implementation on glyph list page
    - add assets
    - update json asset

--- lib/glyph-list.dart
+++ lib/glyph-list.dart
@@ -1,6 +1,10 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:video_player/video_player.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'dart:convert';
+import 'dart:developer';
 
 class GlyphListWidget extends StatefulWidget {
 
@@ -12,9 +16,9 @@ class GlyphListWidget extends StatefulWidget {
 }
 
 class _GlyphList extends State<GlyphListWidget> {
-  Future<List<dynamic>> _glyphs;
+  late Future<List<dynamic>> _glyphs;
 
-  AudioCache _audioCache;
+  late AudioCache _audioCache;
 
   Future<List<dynamic>> fetchList() async {
     String s = await DefaultAssetBundle.of(context)
@@ -31,8 +35,8 @@ class _GlyphList extends State<GlyphListWidget> {
 
   @override
   void dispose() {
-    _audioCache.fixedPlayer.stop();
-    _audioCache.fixedPlayer.dispose();
+    _audioCache.fixedPlayer?.stop();
+    _audioCache.fixedPlayer?.dispose();
     super.dispose();
   }
 
@@ -41,10 +45,12 @@ class _GlyphList extends State<GlyphListWidget> {
         future: _glyphs,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            var glyphInfo = snapshot.data;
+            dynamic? glyphInfo = snapshot.data;
             return ListView(
               children: <Widget>[
-                for (var glyph in glyphInfo) ListTile(
+                // Should check that the type of glyph
+                // is effectively a Map and contains the keys needed
+                if (glyphInfo != null) for (var glyph in glyphInfo) ListTile(
                     leading: Text(glyph["glyph"],
                         style: TextStyle(fontSize: 39.0,
                             fontWeight: FontWeight.bold,
@@ -53,12 +59,24 @@ class _GlyphList extends State<GlyphListWidget> {
                         style: TextStyle(fontSize: 11.0,
                             fontWeight: FontWeight.normal,
                             color: Colors.blueAccent)),
-                    trailing: IconButton(
-                      color: Colors.orange,
-                      icon: Icon(Icons.play_circle_outline),
-                      onPressed: () => _playShortSound(glyph["audio"]),
-                    )
-                ),
+                    trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                      IconButton(
+                        color: Colors.orange,
+                        icon: Icon(Icons.play_circle_outline),
+                        onPressed: () => _playShortSound(glyph["audio"]),
+                      ),
+                      IconButton(
+                        color: Colors.deepPurple,
+                        icon: Icon(Icons.topic_outlined),
+                        onPressed: () => _showWebContent(glyph["web"]),
+                      ),
+                      IconButton(
+                        color: Colors.teal,
+                        icon: Icon(Icons.videocam_rounded),
+                        onPressed: () => _playStrokeOrderVideo(glyph["video"]),
+                      )
+                    ],
+                    )),
               ],
             );
           } else if (snapshot.hasError) {
@@ -81,10 +99,148 @@ class _GlyphList extends State<GlyphListWidget> {
   }
 
   void _playShortSound(String asset) {
-    _audioCache.fixedPlayer.stop();
-    if (_audioCache.fixedPlayer.state != PlayerState.PLAYING) {
+    _audioCache.fixedPlayer?.stop();
+    if (_audioCache.fixedPlayer?.state != PlayerState.PLAYING) {
       _audioCache.play(asset);
     }
+  }
+
+  void _playStrokeOrderVideo(String filename) {
+    showDialog(context: context, builder: (BuildContext context) {
+      return VideoDialogBox(
+        file: "assets/video/glyphs/" + filename,
+      );
+    });
+  }
+
+  void _showWebContent(String url) {
+    Navigator.push(context, PageRouteBuilder(
+        opaque: false,
+        pageBuilder: (BuildContext context, _, __) {
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('Glyph Info'),
+              ),
+              body: WebView(
+                initialUrl: url,
+                navigationDelegate: (request) {
+                  if (request.url == url) {
+                    // Wikipedia redirects to .m domain for mobile phones,
+                    // so had to change address to the .m domain
+                    return NavigationDecision.navigate;
+                  } else {
+                    // The delegate is triggered for all requests, including the initial one
+                    return NavigationDecision.prevent;
+                  }
+                },
+              )
+          );
+        }
+    ));
+  }
+
+}
+
+class VideoDialogBox extends StatefulWidget {
+  final String file;
+
+  const VideoDialogBox({Key? key, required this.file}) : super(key: key);
+
+  @override
+  _VideoDialogBoxState createState() => _VideoDialogBoxState();
+}
+
+class _VideoDialogBoxState extends State<VideoDialogBox> {
+  late VideoPlayerController _controller;
+  bool _playClicked = false;
+  bool _replayClicked = false;
+  bool _slowPlay = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.asset(widget.file)
+    // cascade operator (not technically an operator, just Dart syntax)
+      ..initialize().then((_) {
+        // autoplay the video as soon as initialized
+        _controller.play();
+      });
+
+    _controller.addListener(() {
+      log("video player state = ${_controller.value}");
+      if (_replayClicked || (_playClicked && (_controller.value.position == _controller.value.duration)) ) {
+        _controller.seekTo(Duration.zero);
+      }
+      _playClicked = false;
+      _replayClicked = false;
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Stroke Order"),
+      content: _controller.value.isInitialized
+          ? AspectRatio(
+        aspectRatio: _controller.value.aspectRatio,
+        child: VideoPlayer(_controller),
+      )
+          : Text("loading..."),
+      actions: [
+        IconButton(
+          onPressed: () {
+            _playClicked = true;
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          },
+          icon: Icon(
+            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            _replayClicked = true;
+            _controller.play();
+          },
+          icon: Icon(
+              Icons.replay
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            if (_slowPlay)
+              _controller.setPlaybackSpeed(1.0);
+            else
+              _controller.setPlaybackSpeed(.33);
+            setState(() {
+              _slowPlay = !_slowPlay;
+            });
+          },
+          icon: Icon(
+              Icons.slow_motion_video
+          ),
+          color: _slowPlay ? Colors.black : Colors.grey,
+        ),
+        IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+              Icons.close
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    log("video player disposed");
+    super.dispose();
   }
 
 }
